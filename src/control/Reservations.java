0ede68aba377804bf67fd1a548eaa13dec402531
Daniel Yee Kheng Tan <danielykt-pm23@student.tarc.edu.my>
2024-12-17 20:42:37 +0800

    push

--- src/control/Reservations.java
+++ src/control/Reservations.java
@@ -1,4 +1,16 @@
 package control;
 
+import boundary.ReservationsUI;
+
 public class Reservations {
+    private ReservationsUI reservationMaintenanceUI = new ReservationsUI();
+    public void runReservationMaintenance() {
+        int choice = 0;
+        do {
+            choice = reservationMaintenanceUI.getMainMenuChoice();
+            switch (choice) {
+
+            }
+        } while (choice != 6);
+    }
 }
